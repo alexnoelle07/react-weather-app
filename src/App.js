f491edb904c45611a3b98a24832210e6fe55c750
alexnoelle07 <64166422+alexnoelle07@users.noreply.github.com>
2021-06-29 15:48:58 -0700

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <div className="container">
       <h1>Weather App</h1>
       <footer>
         This project was coded by Alex Noelle and is {""}
       <a href="https://github.com/alexnoelle07/react-weather-app"target="noreferrer">open-sourced on GitHub</a>
       </footer>
+      </div>
     </div>
   );
 }
